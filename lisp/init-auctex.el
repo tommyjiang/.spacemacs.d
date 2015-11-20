4e7a434bdb0bfdb550e48202e52ac8d400bdb422
Tommy-RMBP <tommy_jiang@foxmail.com>
2015-11-20 14:30:09 +0800

    2015-11-20 修改：init-auctex 改为中文注释

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -1,4 +1,4 @@
-; LaTeX mode hook
+; LaTeX mode 采用 AUCTeX
 (mapc (lambda (mode)
          (add-hook 'LaTeX-mode-hook mode))
          (list 'auto-fill-mode
@@ -6,34 +6,32 @@
                'turn-on-reftex
                'linum-mode))
 
+; AUCTeX 设置
 (add-hook 'LaTeX-mode-hook
               (lambda ()
-                (setq TeX-auto-untabify t     ; remove all tabs before saving
-                      TeX-engine 'xetex       ; use xelatex default
-                      TeX-show-compilation nil) ; display compilation windows
-                (TeX-global-PDF-mode t)       ; enable PDF mode
+                (setq TeX-auto-untabify t     ;
+                      TeX-engine 'xetex       ; 默认采用 xetex
+                      TeX-show-compilation nil) ; 不显示编译信息
+                (TeX-global-PDF-mode t)       ; 打开 PDF 模式
                 (setq TeX-save-query nil)
                 (imenu-add-menubar-index)
                 (setq TeX-command-default "LaTeX")
                 (setq TeX-master "master")
                 (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)))
 
-; Disable auto newline in LaTeX mode
-(add-hook 'LaTeX-mode-hook
-          (lambda ()
-            (set-fill-column 99999)))
-
+; Mint 命令
 (add-hook 'LaTeX-mode-hook
   (lambda ()
     (add-to-list 'TeX-command-list 
       '("Mint" "%`xelatex -shell-escape %(mode)%' %t" TeX-run-TeX nil t))))
 
+; latex 命令
 (add-hook 'LaTeX-mode-hook
   (lambda ()
     (add-to-list 'TeX-command-list 
       '("mk" "latexmk -pdf -xelatex %(mode)%' %t" TeX-run-TeX nil t))))
 
-; double search
+; 双向搜索
 (setq TeX-source-correlate-mode t)
 (setq TeX-source-correlate-method-active 'synctex)
 (setq TeX-source-correlate-start-server t)
@@ -42,7 +40,7 @@
     '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b %n %o")))
 (server-start)
 
-; normal font size
+; 不放大缩小字号
 (setq font-latex-fontify-sectioning 1.0) ; do not fontify section title
 (setq font-latex-fontify-script nil) ; do not fontify script
 
