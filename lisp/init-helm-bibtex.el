0899f7fc020c63e6a3ef46ef12979bf24233c154
Tommy-RMBP <tommy_jiang@foxmail.com>
2018-03-10 18:57:09 +0800

    2018-03-10 helm-bibtex 配置更新

--- lisp/init-helm-bibtex.el
+++ lisp/init-helm-bibtex.el
@@ -1,3 +1,11 @@
+; helm-bibtex settings
+(setq bibtex-completion-bibliography '("~/org/paper.bib"
+                                       "~/org/tutorial.bib"))
+(setq bibtex-completion-library-path '("~/Documents/Library.papers3/Library.papers3/Files" "~/Downloads/Machine Learning/Books/General" "~/Downloads/Machine Learning/Tutorials"))
+
+; Org export
+(setq org-latex-pdf-process (list "latexmk -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f %f"))
+
 ; Replaced version of open pdf
 (defun my/org-ref-open-pdf-at-point ()
     "Open the pdf for bibtex key under point if it exists."
