b5a5c21188b4a8d7950f1a56fa0d9fb0caec2d7c
Tommy-RMBP <tommy_jiang@foxmail.com>
2018-04-10 11:52:45 +0800

    Update auctex

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -36,8 +36,7 @@
 (setq TeX-source-correlate-method-active 'synctex)
 (setq TeX-source-correlate-start-server t)
 (setq TeX-view-program-selection '((output-pdf "Skim")))
-(setq TeX-view-program-list
-    '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b %n %o")))
+(setq TeX-view-program-list '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -b %n %o")))
 (server-start)
 
 ; 不放大缩小字号
