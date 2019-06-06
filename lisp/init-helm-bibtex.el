4140ad507b609135754e4a5993502125fa981879
Tommy-RMBP <tommy_jiang@foxmail.com>
2019-06-06 11:15:39 +0800

    helm-bibtex recursive 加入目录

--- lisp/init-helm-bibtex.el
+++ lisp/init-helm-bibtex.el
@@ -1,11 +1,25 @@
 ; helm-bibtex settings
 (setq bibtex-completion-bibliography '("~/org/paper.bib"
                                        "~/org/tutorial.bib"))
-(setq bibtex-completion-library-path '("~/Downloads/Machine Learning/Papers"
-                                       "~/Downloads/Machine Learning/Books"
-                                       "~/Documents/Books/epub"
-                                       "~/Documents/Books/PDF"
-                                       "~/Downloads/Machine Learning/Tutorials"))
+
+(defun list-dirs-recursively (dir &optional include-symlinks)
+  "Return list of all subdirectories recursively. Returns absolute paths.
+Optionally call recursively on symlinks."
+  (let ((result nil)
+        (tramp-mode (and tramp-mode (file-remote-p (expand-file-name dir)))))
+    (dolist (file (file-name-all-completions "" dir))
+      (when (and (directory-name-p file) (not (member file '("./" "../"))))
+        (setq result (nconc result (list (expand-file-name file dir))))
+        (let* ((leaf (substring file 0 (1- (length file))))
+               (full-file (expand-file-name leaf dir)))
+          ;; Don't follow symlinks to other directories.
+          (unless (and (file-symlink-p full-file) (not include-symlinks))
+            (setq result
+                  (nconc result (list-dirs-recursively full-file)))))
+        ))
+    result))
+
+(setq bibtex-completion-library-path (list-dirs-recursively "~/Downloads/Machine Learning"))
 
 ; Org export
 (setq org-latex-pdf-process (list "latexmk -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f %f"))
