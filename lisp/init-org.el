98aae686341c9b5fb835410078d8b607feb77c22
Tommy-iMac <tommy_jiang@foxmail.com>
2024-09-01 15:48:00 +0800

    remove 多余 org modules

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -951,9 +951,7 @@ Late deadlines first, then scheduled, then non-late deadlines"
 (setq org-clock-sound "/usr/local/lib/tngchime.wav")
 
 ; Enable habit tracking (and a bunch of other modules)
-(setq org-modules (quote (org-bibtex
-                          org-crypt
-                          org-drill
+(setq org-modules (quote (org-crypt
                           org-gnus
                           org-id
                           org-info
